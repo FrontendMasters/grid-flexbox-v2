b029768678f3d2419413d413977ac0a1f6a59029
Joshua Cornett <joshua.m.cornett@proton.me>
2023-08-06 17:36:11 -0700

    Exercise 4a complete!
    Exercise 4a complete!

--- day-1-flexbox/4-flexbox-exercises/4a-collections/begin/js/script.js
+++ day-1-flexbox/4-flexbox-exercises/4a-collections/begin/js/script.js
@@ -1,67 +1,23 @@
-
-
-function include(filename, onload) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = filename;
-    script.type = 'text/javascript';
-    script.onload = script.onreadystatechange = function() {
-        if (script.readyState) {
-            if (script.readyState === 'complete' || script.readyState === 'loaded') {
-                script.onreadystatechange = null;                                                  
-                onload();
-            }
-        } 
-        else {
-            onload();          
-        }
-    };
-    head.appendChild(script);
+const isSameDomain = (styleSheet) => {
+if (!styleSheet.href) {
+    return true;
 }
 
-
-/*
- Check if the stylesheet is internal or hosted on the current domain.
- If it isn't, attempting to access sheet.cssRules will throw a cross origin error.
- See https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet#Notes
- 
- NOTE: One problem this could raise is hosting stylesheets on a CDN with a
- different domain. Those would be cross origin, so you can't access them.
-*/
-const isSameDomain = (styleSheet) => {
-    // Internal style blocks won't have an href value
-    if (!styleSheet.href) {
-      return true;
-    }
-  
-    return styleSheet.href.indexOf(window.location.origin) === 0;
+return styleSheet.href.indexOf(window.location.origin) === 0;
 };
 
-/*
-Determine if the given rule is a CSSStyleRule
-See: https://developer.mozilla.org/en-US/docs/Web/API/CSSRule#Type_constants
-*/
 const isStyleRule = (rule) => rule.type === 1;
 
-/**
- * Get all custom properties on a page
- * @return array<array[string, string]>
- * ex; [["--color-accent", "#b9f500"], ["--color-text", "#252525"], ...]
- */
 const getCSSCustomPropIndex = () =>
-// styleSheets is array-like, so we convert it to an array.
-// Filter out any stylesheets not on this domain
 [...document.styleSheets].filter(isSameDomain).reduce(
     (finalArr, sheet) =>
     finalArr.concat(
-        // cssRules is array-like, so we convert it to an array
         [...sheet.cssRules].filter(isStyleRule).reduce((propValArr, rule) => {
         const props = [...rule.style]
             .map((propName) => [
             propName.trim(),
             rule.style.getPropertyValue(propName).trim()
             ])
-            // Discard any props that don't start with "--". Custom props are required to.
             .filter(([propName]) => propName.indexOf("--") === 0);
 
         return [...propValArr, ...props];
@@ -70,18 +26,67 @@ const getCSSCustomPropIndex = () =>
     []
 );
 
-include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
-    $(document).ready(function() {
-        let cssCustomPropIndex = getCSSCustomPropIndex();
-        console.log(cssCustomPropIndex);
 
-        let i = 0;
-        //cssCustomPropIndex.forEach((item,index)=>{console.log(`${index}` + "'s value is: " +`${item[1]}`)});
-        $("figure").each(function(){
-            //console.log($(this).css());
-            $(this).css("color", cssCustomPropIndex[i][1]);
-            $(this).find("figcaption > a").css("color", cssCustomPropIndex[i][1]);
-            i++;
-        });
+const padZero = (str, len) => {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+};
+
+const invertColor = (hex, bw) => {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+        g = parseInt(hex.slice(2, 4), 16),
+        b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+        // https://stackoverflow.com/a/3943023/112731
+        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+            ? '#000000'
+            : '#FFFFFF';
+    }
+    // invert color components
+    r = (255 - r).toString(16);
+    g = (255 - g).toString(16);
+    b = (255 - b).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + padZero(g) + padZero(b);
+};
+
+$(document).ready(function() {
+    let cssCustomPropIndex = getCSSCustomPropIndex();
+    let i = 3;
+
+    //if getting the root variables failed (thanks Firefox/CORS), enter them manually
+    if(cssCustomPropIndex.length == 0) {
+        cssCustomPropIndex = [
+            ['--color1', '#001219'],
+            ['--color2', '#005f73'],
+            ['--color3', '#3d9397'],
+            ['--color4', '#94d28d'],
+            ['--color5', '#e9d8a6'],
+            ['--color6', '#ee9800'],
+            ['--color7', '#ca6702'],
+            ['--color8', '#bb3e03'],
+            ['--color9', '#ae2012'],
+            ['--color10', '#9b2226']
+        ];
+    }
+    let n = 0;
+
+    $("figure").each(function(){
+        i = Math.floor(Math.random()*cssCustomPropIndex.length);
+        $(this).css("border-color", cssCustomPropIndex[i][1]);
+        //$(this).find("figcaption").css("background", invertColor(cssCustomPropIndex[i][1], 1));
+        //$(this).find("figcaption > a").css("color", cssCustomPropIndex[i][1]);
+        $(this).find("figcaption > a").css("color", cssCustomPropIndex[0][1]);
     });
 });
